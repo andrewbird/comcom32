9856c644f71abf817be057f57f0def26b31d5fe3
Andrew Bird <ajb@spheresystems.co.uk>
2026-07-20 20:06:58 +0100

    CI: Use the test scripts from Dosemu
    1/ Used the packaged build
    2/ Publish from the packages
    3/ Test comcom32 and comcom64 in separate steps

--- ci_test.sh
+++ ci_test.sh
@@ -1,15 +1,35 @@
-#!/bin/sh
+#!/usr/bin/bash
 
-set -e
+VERSION="${1}"
 
-if ! dosemu -td -o boot.log -E ver ; then
-  {
-    echo "================== boot.log ==================="
-    cat boot.log
-    echo "==============================================="
-  } >&2
-  exit 1
-fi
+set +e
 
-make both -j 9
-ls -l src/32/comcom32.exe
+export TEST_DOSEMU=/usr/bin/dosemu
+export TEST_CMDDIR=/usr/share/dosemu/dosemu2-cmds-0.3
+export NO_FAILFAST=1
+
+export COPY_COMMAND_COM=/usr/share/comcom${VERSION}/comcom${VERSION}.exe
+cat >&2 << EOF
+=====================================================
+=      Tests run on emulated CPU, KVM and VM86      =
+=====================================================
+EOF
+python3 test/test_processor.py
+
+cat >&2 << EOF2
+=====================================================
+=        Tests run on various flavours of DOS       =
+=====================================================
+EOF2
+python3 test/test_dosemu.py PPDOSGITTestCase
+mkdir comcom${VERSION}
+for i in test_*.*.*.log ; do
+  test -f $i && mv $i comcom${VERSION}/.
+done
+
+# Return non-zero if any logfiles were generated
+for i in comcom${VERSION}/test_*.*.*.log ; do
+  test -f $i || exit 0
+done
+
+exit 1
